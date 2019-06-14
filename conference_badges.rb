a9a877693825859dc8114f7cd1c470f327a58ef0
Yosef Lehrer <yolehrer@gmail.com>
2019-06-14 20:59:21 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def printer(attendees)
  batch_badge_creator(attendees).each do |attendee|
    puts attendee
  end
- assign_rooms.each(attendees) do |attendee|
+ assign_rooms(attendees).each do |attendee|
    puts attendee
  end
 end
